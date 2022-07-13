3d3f6d06e68bca66b13bebaf06083844417776a5
francomoreira <108966480+francomoreira@users.noreply.github.com>
2022-07-13 16:40:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var usuario = prompt('cual es tu peso?')
+var peso = parseInt(usuario);
+var gTierra = 9.8;
+var gMarte = 3.7;
+var gJupiter = 24.8;
+var pesoFinal;
+pesoFinal = usuario * gMarte / gTierra;
+pesoFinal = parseInt(pesoFinal);
+document.write('Tu peso en marte es: <strong>' +pesoFinal+' Kg.</strong>');
